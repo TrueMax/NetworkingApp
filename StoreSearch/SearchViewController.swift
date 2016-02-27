bc83e1f88917ad9eb881972118a2a0db5500d9e4
TrueMax <maximnatural@gmail.com>
2016-02-28 00:31:44 +0300

    search bar install

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class SearchViewController: UIViewController {
+    
+    // MARK - Properties 
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0)
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,3 +29,6 @@ class SearchViewController: UIViewController {
 
 }
 
+extension SearchViewController: UISearchBarDelegate {
+
+}
