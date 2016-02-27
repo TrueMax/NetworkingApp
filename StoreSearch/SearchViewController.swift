8891b372ea9bcf50cb27f8c16b4fa8f53b0cda35
TrueMax <maximnatural@gmail.com>
2016-02-28 01:08:59 +0300

    bar top attached keyboard dismissed interactively

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -10,11 +10,11 @@ import UIKit
 
 class SearchViewController: UIViewController {
     
-    // MARK - Properties 
+    // MARK: - Properties
     
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
-    
+    var searchResults = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,5 +30,39 @@ class SearchViewController: UIViewController {
 }
 
 extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        searchResults = [String]()
+        for i in 0...2 {
+            searchResults.append(String(format: "Some results %d for '%@'", i, searchBar.text!))
+        }
+        tableView.reloadData()
+    }
+    
+    // наводим красоту 
+    func positionForBar(bar: UIBarPositioning) -> UIBarPosition {
+        return .TopAttached
+    }
+
+}
+
+extension SearchViewController: UITableViewDataSource {
+    // метод объявляется без override, потому что root view controller = UIViewController, а не TableViewController (где родительские методы надо переопределять)
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searchResults.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cellId = "SearchResultCell"
+        var cell: UITableViewCell! = tableView.dequeueReusableCellWithIdentifier(cellId)
+        if cell == nil {
+            cell = UITableViewCell(style: .Default, reuseIdentifier: cellId)
+        }
+        cell.textLabel!.text = searchResults[indexPath.row]
+        return cell
+    }
+}
+
+extension SearchViewController: UITableViewDelegate {
 
 }
