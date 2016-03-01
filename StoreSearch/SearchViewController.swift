2aa4d9fb47add305e08b3b4f09507a7dce88ea7b
TrueMax <maximnatural@gmail.com>
2016-03-01 19:09:13 +0300

    Tuned iTunes search. Works correctly.

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -42,18 +42,25 @@ class SearchViewController: UIViewController {
 
 extension SearchViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        
+        if !searchBar.text!.isEmpty {
+        
         searchBar.resignFirstResponder()
-        searchResults = [SearchResult]()
         hasSearched = true
-        if searchBar.text! != "justin bieber" {
-            for i in 0...2 {
-                let searchResult = SearchResult()
-                searchResult.name = String(format: "Some results %d for ", i)
-                searchResult.artistName = searchBar.text!
-                searchResults.append(searchResult)
+        searchResults = [SearchResult]()
+        
+        let url = urlSearchWithText(searchBar.text!)
+            print("URL: '\(url)'")
+            if let jsonString = performStoreRequestWithURL(url) {
+                print("Received JSON string '\(jsonString)'")
+                if let dictionary = parseJSON(jsonString) {
+                    print("Dictionary \(dictionary)")
+                    searchResults = parseDictionary(dictionary)
+                    tableView.reloadData()
+                    return
+                }
             }
-            
-            tableView.reloadData()
+            showNetworkError()
         }
     }
     
@@ -62,6 +69,94 @@ extension SearchViewController: UISearchBarDelegate {
         return .TopAttached
     }
 
+    
+    //MARK: - HTTP Request / Response
+    func urlSearchWithText(searchText: String) -> NSURL {
+        let escapedSearchText = searchText.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())!
+        let urlString = String(format: "https://itunes.apple.com/search?term=%@", escapedSearchText)
+        let url = NSURL(string: urlString)
+        return url!
+    }
+    
+    func performStoreRequestWithURL(url: NSURL) -> String? {
+        do {
+            return try String(contentsOfURL: url, encoding: NSUTF8StringEncoding)
+        } catch {
+            print("Download error: \(error)")
+            return nil
+        }
+    }
+    
+    func parseJSON(jsonString: String) -> [String: AnyObject]? {
+        guard let data = jsonString.dataUsingEncoding(NSUTF8StringEncoding)
+            else {return nil}
+        do {
+            return try NSJSONSerialization.JSONObjectWithData(data, options: []) as? [String: AnyObject]
+        } catch {
+            print("JSON error \(error)")
+            return nil
+        }
+    }
+    
+    func parseDictionary (dictionary: [String: AnyObject]) -> [SearchResult] {
+        guard let array = dictionary["results"] as? [AnyObject] // надо убедиться, что ключ "results" есть в словаре и записаться в массив словарей
+            else {
+                print("Expected 'results' array")
+                return [] }
+        
+        // парсинг массива словарей, downcast AnyObject -> String обязательно
+        var searchResults = [SearchResult]()
+        
+        
+        for resultDict in array {
+            if let resultDict = resultDict as? [String: AnyObject] {
+                var searchResult: SearchResult?
+                if let wrapperType = resultDict["wrapperType"] as? String {
+                    switch wrapperType {
+                    case "track":
+                        searchResult = parseTrack(resultDict)
+                    default: break
+                    }
+                }
+                if let result = searchResult {
+                    searchResults.append(result)
+                }
+                
+            }
+        }
+        return searchResults
+    }
+    
+    func parseTrack (dictionary: [String: AnyObject]) -> SearchResult {
+        let searchResult = SearchResult()
+        searchResult.name = dictionary["trackName"] as! String
+        searchResult.artistName = dictionary["artistName"] as! String
+        
+        searchResult.artworkURL60 = dictionary["artworkUrl60"] as! String
+        
+        searchResult.artworkURL100 = dictionary["artworkUrl100"] as! String
+        
+        // searchResult.storeURL = dictionary["storeUrl"] as! String
+            
+        searchResult.kind = dictionary["kind"] as! String
+            
+        searchResult.currency = dictionary["currency"] as! String
+        
+        if let price = dictionary["trackPrice"] as? Double {
+            searchResult.price = price
+        }
+        if let genre = dictionary["primaryGenreName"] as? String {
+            searchResult.genre = genre
+        }
+        return searchResult
+    }
+    
+    func showNetworkError() {
+        let alert = UIAlertController(title: "Network error", message: "Error reading from iTunes Store. Try again later", preferredStyle: .Alert)
+        let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
+        alert.addAction(action)
+        presentViewController(alert, animated: true, completion: nil)
+    }
 }
 
 extension SearchViewController: UITableViewDataSource {
